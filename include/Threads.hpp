c999a9a2c6d7e25b4674ecefe7efd536224e9ad3
Martynov Stas <47531255+piligr1m@users.noreply.github.com>
2019-12-28 16:23:36 +0300

    Update Threads.hpp

--- include/Threads.hpp
+++ include/Threads.hpp
@@ -12,20 +12,17 @@
 #include <string>
 #include <thread>
 
-namespace thread
-    {
 class c_task
 {
 public:
     c_task(std::string id);
-    
+private:
+    std::string generate_prototype(int length);
+    std::string calc_hash(std::string &prot);
+    int get_random(int minimum, int maximum);
+    char get_rand_char();
+    void check_hash(std::string &hash);
 private:
     std::string m_id;
 };
-    std::string generate_prototype(int length);
-    std::string calc_hash(std::string& prototype);
-    int get_random(int minimum, int maximum);
-    char get_rand_char();
-    void check_hash(std::string& hash);
-    }
 #endif /* Threads_hpp */
